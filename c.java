29b61209ec779e3c8458420b4b10afe415326a38
rosekao871128 <56061923+rosekao871128@users.noreply.github.com>
2019-11-06 16:19:18 +0800

    Add files via upload

---
+++ c.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class c{
+    public static void main (String[]args){
+        Scanner scn = new Scanner(System.in);
+        long n =scn.nextLong();
+        int sum = 0;
+        int k = 0;
+        while(n>0){
+            k++;
+            sum+=n%10;
+            n=n/10;
+
+        }
+        System.out.println(k);
+        System.out.println(sum);
+    }
+}
